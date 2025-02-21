16d3a7c850eedc052f8bc451ff32b0f885c09417
SergioProgramando01 <104284959+SergioProgramando01@users.noreply.github.com>
2025-02-20 21:46:03 -0500

    Add files via upload

---
+++ Projects/src/projects/Ejercicio5.java
@@ -0,0 +1,29 @@
+package projects;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public void correrEjercicio5(){
+        
+        Scanner datos = new Scanner(System.in);
+        
+        int temperatura;
+        int piso;
+        boolean rango;
+        
+        System.out.println("Ingrese temperatura en °C: ");
+        temperatura = datos.nextInt();
+        
+        System.out.println("Ingrese piso: ");
+        piso = datos.nextInt();
+        
+        rango = (temperatura >= 18 && temperatura <= 25 );
+        
+        if(rango){
+            System.out.println("El ascensor se mueve al piso " + piso + ".");
+        }
+        else {
+            System.out.println("La temperatura de " + temperatura + " no es adecuada y no permite el movimiento del ascensor");
+        }  
+    }    
+}
---
+++ Projects/src/projects/projects.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projects;
+
+/**
+ *
+ * @author SERGIO
+ */
+public class projects {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ejercicio5 ejercicio5 = new Ejercicio5();
+        ejercicio5.correrEjercicio5();
+
+    }
+    
+}
